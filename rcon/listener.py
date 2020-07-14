bbb5af8e8dad490e5b4b78a866f36163485dc713
Nylonicious <50183564+nylonicious@users.noreply.github.com>
2020-07-14 20:10:22 +0200

    Add player event handlers

--- rcon/listener.py
+++ rcon/listener.py
@@ -1,8 +1,10 @@
 import asyncio
+from typing import List
 
 from loguru import logger
+from pydantic import ValidationError
 
-from . import exceptions
+from . import exceptions, models
 from .client import Client
 
 
@@ -25,11 +27,30 @@ class Listener(Client):
         await asyncio.gather(*tasks)
 
     async def server_event_loop(self):
+        event_handlers = {
+            "player.onJoin": self._handle_player_on_join,
+            "player.onAuthenticated": self._handle_player_on_auth,
+            "player.onDisconnect": self._handle_player_on_disconnect,
+            "player.onLeave": self._handle_player_on_leave,
+            "player.onKill": self._handle_player_on_kill,
+            # "player.onChat":
+            # "player.onSquadChange"
+            # "player.onTeamChange":
+            # "punkBuster.onMessage":
+            # "server.onMaxPlayerCountChange":
+            # "server.onLevelLoaded":
+            # "server.onRoundOver":
+            # "server.onRoundOverPlayers":
+            # "server.onRoundOverTeamScores":
+        }
         while True:
             try:
                 event = await self._protocol.listen()
-                logger.debug(event)
+                await event_handlers[event[0]](event)
             except asyncio.TimeoutError:
+                pass
+            except KeyError:
+                # logger.error(f"{event} it's not a valid event {self.ip}:{self.port}")
                 pass
 
     async def server_info_loop(self):
@@ -39,3 +60,35 @@ class Listener(Client):
                 await asyncio.sleep(10)
             except exceptions.RCONException:
                 await self.reconnect()
+
+    async def _handle_player_on_join(self, event: List[str]):
+        try:
+            on_join = models.PlayerOnJoin(player_name=event[1], player_guid=event[2])
+        except ValidationError:
+            return
+
+    async def _handle_player_on_auth(self, event: List[str]):
+        try:
+            on_auth = models.PlayerOnAuthenticated(player_name=event[1])
+        except ValidationError:
+            return
+
+    async def _handle_player_on_disconnect(self, event: List[str]):
+        try:
+            on_disconnect = models.PlayerOnDisconnect(player_name=event[1], reason=event[2])
+        except ValidationError:
+            return
+
+    async def _handle_player_on_leave(self, event: List[str]):
+        try:
+            on_leave = models.PlayerOnLeave()
+        except ValidationError:
+            return
+
+    async def _handle_player_on_kill(self, event: List[str]):
+        try:
+            on_kill = models.PlayerOnKill(
+                killer_name=event[1], victim_name=event[2], weapon_key=event[3], is_hs=event[4]
+            )
+        except ValidationError:
+            return
